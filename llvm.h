bb2570e257d5eb445b7665c3b56968eaba8dbb22
KinryuBarao <s.franciscobarao@gmail.com>
2019-05-22 17:08:48 +0100

    ups forgot llvm.h

---
+++ llvm.h
@@ -0,0 +1,13 @@
+#include "structures.h"
+#include "semantics.h"
+#include "functions.h"
+
+
+void produce(Structure *node, char* scope_name,int* count_label, int* count );
+const char* type_to_llvm(basic_type t );
+void produce_header(Structure* node, char* scope_name);
+void produce_declarations(char* scope_name);
+void produce_statement(Structure* node, char* scope_name, int* count_label,int* count);
+void produce_assign(Structure* node, char* scope_name,int* count);
+char* produce_expression(Structure* node, char* scope_name, int* count);
+
